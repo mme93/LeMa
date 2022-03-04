8d3a34f2dca6142bfdcbb7f8670c9ab1f7970456
magantus@googlemail.com <Extrem93!>
2022-03-04 20:15:45 +0100

    First Commit

---
+++ src/main/java/de/LeMa/LeMaApplication.java
@@ -0,0 +1,13 @@
+package de.LeMa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeMaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeMaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/LeMa/LeMaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.LeMa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
